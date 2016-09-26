f82f512ffd1f490df3ddecbf5601ad9ce807414c
Rachel Sharp <ras.beckman@gmail.com>
2016-09-26 13:04:45 +0100

    Added Upstart support to start and stop playlist-bot as a service

--- connections/reddit.py
+++ connections/reddit.py
@@ -15,6 +15,7 @@ r.login(login_details["reddit_username"], login_details["reddit_password"])
 
 def get_hot_submissions(subreddit_name, n=25, retry_count=0):
     try:
+        print "test test test"
         subreddit = r.get_subreddit(subreddit_name)
         return subreddit.get_hot(limit=n)
     except Exception as e:
