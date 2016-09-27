4f537bdf3071d26c2ff8dd5af69c39e4430f2872
Rachel Sharp <ras.beckman@gmail.com>
2016-09-27 13:07:58 +0100

    Changes to logging

--- playlist_bot.py
+++ playlist_bot.py
@@ -18,9 +18,6 @@ MINIMUM_PLAYLIST_SIZE = 5
 def has_video_content(submission):
     """ Return boolean based on whether submission is viable to have a youtube playlist made """
 
-    logging.info("--- EXECUTING FUNCTION HAS_VIDEO_CONTENT ---")
-    logging.info("submission title: " + submission.title)
-
     all_comments = submission.comments
     # imported from praw.helpers
     flat_comments = flatten_tree(all_comments)
@@ -31,20 +28,16 @@ def has_video_content(submission):
         if type(comment) != MoreComments and comment.score >= MINIMUM_COMMENT_UPVOTES:
             links_in_comment = YOUTUBE_REGEX.findall(comment.body)
             if links_in_comment:
-                logging.debug("found youtube links in a comment")
                 video_count = video_count + len(links_in_comment)
         if video_count >= MINIMUM_PLAYLIST_SIZE:
-            logging.debug("video_count greater than threshold: " + str(video_threshold))
             return True
-    logging.debug("video_count: " + str(video_count) + " less than threshold: " + str(video_threshold))
     return False
 
 class Playlist(object):
     """ A youtube playlist based off of a reddit submission """
     def __init__(self, submission):
         super(Playlist, self).__init__()
-        logging.info("--- INITIALIZING NEW PLAYLIST INSTANCE ---")
-        logging.info("submission title: " + submission.title)
+
         if len(submission.title) <= 60:
             self.title = submission.title
         else:
@@ -59,7 +52,6 @@ URL: %s
 Created automatically by playlist-bot
         """ % (submission.title, submission.url, submission.selftext)
 
-        logging.info("using youtube api to insert playlist information")
         playlist_insert_response = youtube.youtube.playlists().insert(
           part="snippet,status",
           body=dict(
@@ -73,8 +65,6 @@ Created automatically by playlist-bot
           )
         ).execute()
         self.id = playlist_insert_response["id"]
-        logging.debug("got playlist insert response:")
-        logging.debug(playlist_insert_response)
 
         submission.has_playlist = True
 
@@ -83,11 +73,7 @@ Created automatically by playlist-bot
     def add_video(self, video_id):
         """ For a video id, add it to the list of videos in the playlist """
 
-        logging.info("--- EXECUTING FUNCTION ADD_VIDEO FOR CLASS PLAYLIST ---")
-        logging.info("playlist_title: " + self.title + ", video_id: " + video_id)
-
         try:
-            logging.info("using youtube api to insert a new playlistitem")
             playlistitems_insert_response = youtube.youtube.playlistItems().insert(
               part="snippet,status",
               body=dict(
@@ -100,8 +86,6 @@ Created automatically by playlist-bot
                 )
               )
             ).execute()
-            logging.debug("got playlistitems insert response:")
-            logging.debug(playlistitems_insert_response)
 
             self.videos.append(video_id)
 
@@ -113,24 +97,18 @@ class PlaylistBot(object):
     def __init__(self):
         super(PlaylistBot, self).__init__()
 
-        logging.info("--- INITIALIZING NEW PLAYLISTBOT INSTANCE ---")
-
         self.submissions = [] # list of submissions
 
         if os.path.isfile("playlists.p"):
-            logging.info("pickled playlists file exists")
+            logging.info("Loading Playlist Dictionary from Pickle")
             with open('playlists.p', 'rb') as fp:
                 self.playlists = pickle.load(fp)
-                logging.debug("contents of playlists.p:")
-                logging.debug(self.playlists)
         else:
-            logging.info("pickled playlists file doesn't exist")
+            logging.info("Creating Playlist Dictionary")
             self.playlists = {} # dict of playlists indexed by submission
 
     def run(self):
         """ Automatically execute a list of tasks at regular intervals """
-
-        logging.info("--- EXECUTING FUNCTION RUN FOR CLASS PLAYLISTBOT ---")
 
         schedule.every(3).minutes.do(self.add_new_submissions)
         schedule.every(3).minutes.do(self.remove_old_submissions)
@@ -139,9 +117,8 @@ class PlaylistBot(object):
         schedule.every(18).minutes.do(self.update_playlists)
         schedule.every(3).minutes.do(self.save)
 
-        logging.info("starting main loop in playlistbot run function")
         while True:
-            logging.info("*** SCHEDULE.RUN_PENDING() *** " + str(time.time()))
+            logging.info("Running pending tasks at " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
             schedule.run_pending()
             time.sleep(60)
 
@@ -150,44 +127,32 @@ class PlaylistBot(object):
     def add_new_submissions(self):
         """ Add newly created reddit submissions to list of watched submissions """
 
-        logging.info("--- EXECUTING FUNCTION ADD_NEW_SUBMISSIONS FOR CLASS PLAYLISTBOT ---")
-
         # submissions created before this shouldn't be watched
         # time now - 12h for timeout
         timeout = time.time() - 43200
-        logging.debug("timeout: " + str(timeout))
 
-        logging.debug("starting main loop for adding new submissions")
-        logging.debug(str(self.submissions))
-        logging.debug(str(self.playlists.keys()))
         for submission in reddit.get_hot_submissions("AskReddit"):
 
-            logging.debug("name:" + submission.name)
-            logging.debug("title:" + submission.title)
             if submission not in self.submissions and submission not in self.playlists.keys():
                 if  timeout < submission.created:
                     self.submissions.append(submission)
-                    logging.info("adding " + submission.title)
-        logging.debug("finished adding new submissions")
+                    logging.info("Adding Submission - " + submission.title)
 
     def remove_old_submissions(self):
         """ Remove old reddit submissions from list of watched submissions """
 
-        logging.info("--- EXECUTING FUNCTION REMOVE_OLD_SUBMISSIONS FOR CLASS PLAYLIST_BOT ---")
-
         # time now - 12h for timeout
         timeout = time.time() - 43200
-        logging.debug("timeout: " + str(timeout))
 
         for submission in self.submissions:
             if timeout > submission.created:
                 self.submissions.remove(submission)
-                logging.info("removing " + submission.title)
+                logging.info("Removing Submission - " + submission.title)
 
     def refresh_submissions(self):
         """ Update the submission information, which among other things gets the latest comments """
 
-        logging.info("--- EXECUTING FUNCTION REFRESH_SUBMISSIONS FOR CLASS PLAYLISTBOT ---")
+        logging.info("Refreshing Submissions")
 
         for submission in self.submissions:
             submission.refresh()
@@ -195,15 +160,12 @@ class PlaylistBot(object):
     def create_playlists(self):
         """ Called to check all submissions in watchlist to see if they should have a playlist created """
 
-        logging.info("--- EXECUTING FUNCTION CREATE_PLAYLISTS FOR CLASS PLAYLISTBOT ---")
-
         for submission in [s for s in self.submissions if s.name not in self.playlists.keys() ]:
             if has_video_content(submission):
+                logging.info("Creating Playlist for Submission - " + submission.title)
                 new_playlist = self.create_playlist(submission)
-                logging.info("created playlist for: " + submission.title )
                 #submission.add_comment("comment text")
                 self.playlists[submission.name] = new_playlist
-                logging.debug("self.playlists[" + submission.name + "] = " + str(self.playlists[submission.name]))
 
     def create_playlist(self, submission):
         """ For a reddit submission, create an associated youtube playlist """
@@ -225,6 +187,7 @@ class PlaylistBot(object):
 
         new_playlist = Playlist(submission)
         for video_id in youtube_links:
+            logging.info("Adding Video " + str(video_id) + " to Playlist - " + submission.title)
             new_playlist.add_video(video_id)
 
         return new_playlist
@@ -232,7 +195,7 @@ class PlaylistBot(object):
     def update_playlists(self, update_all=False):
         """ Checks all threads with playlists to see if their content should be updated """
 
-        logging.info("--- EXECUTING FUNCTION UPDATE_PLAYLISTS FOR CLASS PLAYLISTBOT ---")
+        logging.info("Updating Playlists")
 
         for submission_name, playlist in self.playlists.iteritems():
             youtube_links = []
@@ -253,13 +216,13 @@ class PlaylistBot(object):
                 # add new ones
                 for video_id in youtube_links:
                     if video_id not in playlist.videos:
-                        logging.info("adding new video: " + video_id)
+                        logging.info("Adding Video " + str(video_id) + " to Playlist - " + submission.title)
                         playlist.add_video(video_id)
 
     def save(self):
         """ Pickles the dictionary of playlist data for if execution of this program is restarted """
 
-        logging.info("--- EXECUTING FUNCTION SAVE FOR CLASS PLAYLISTBOT ---")
+        logging.info("Saving a Pickle")
 
         with open('playlists.p', 'wb') as fp:
             pickle.dump(self.playlists, fp)
@@ -272,11 +235,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     logging.getLogger('').handlers = []
-    logging.basicConfig(filename = "logs/playlist_bot_%d.log" % time.time(), filemode="w", level = logging.DEBUG if args.debug else logging.WARNING)
+    logging.basicConfig(filename = "logs/playlist_bot_%d.log" % time.time(), filemode="w", level = logging.DEBUG if args.debug else logging.INFO)
 
     playlist_bot = PlaylistBot()
-    logging.info("--- STARTING PLAYLIST BOT ---")
-    logging.info("--run-once: " + str(args.run_once) + ", --debug: " + str(args.debug))
+    logging.info("Logs for playlist_bot with command line flags:" + (" -ro " if args.run_once else "") + (" -d " if args.debug else ""))
     if args.run_once:
         playlist_bot.add_new_submissions()
         playlist_bot.remove_old_submissions()
@@ -284,5 +246,6 @@ if __name__ == "__main__":
         playlist_bot.create_playlists()
         playlist_bot.update_playlists()
         playlist_bot.save()
+        logging.info("Finished Single Run")
     else:
         playlist_bot.run()
